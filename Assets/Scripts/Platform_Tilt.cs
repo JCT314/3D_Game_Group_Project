fe1973b54a8bd139821a693f928f8f84a01ddbc0
Ernesto Sanchez <esanchez9@sdsu.edu>
2020-11-30 21:29:24 -0800

    make the keys less sensitive

--- Assets/Scripts/Platform_Tilt.cs
+++ Assets/Scripts/Platform_Tilt.cs
@@ -13,19 +13,19 @@ public class Platform_Tilt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis ("Horizontal") > .2)
+        if(Input.GetAxis ("Horizontal") > .3)
         {
             transform.Rotate(0, 0, 1);
         }
-        if (Input.GetAxis("Horizontal") < -.2)
+        if (Input.GetAxis("Horizontal") < -.3)
         {
             transform.Rotate(0, 0, -1);
         }
-        if (Input.GetAxis("Vertical") > .2)
+        if (Input.GetAxis("Vertical") > .3)
         {
             transform.Rotate(1, 0, 0);
         }
-        if (Input.GetAxis("Vertical") < -.2)
+        if (Input.GetAxis("Vertical") < -.3)
         {
             transform.Rotate(-1, 0, 0);
         }
