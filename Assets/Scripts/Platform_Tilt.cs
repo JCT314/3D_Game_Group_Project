6c8c97da8d72c8f88258923126b7e13033878f08
Ernesto Sanchez <esanchez9@sdsu.edu>
2020-12-01 10:46:31 -0800

    changed the keypress down sensitivity in order for the platform to tilt better

--- Assets/Scripts/Platform_Tilt.cs
+++ Assets/Scripts/Platform_Tilt.cs
@@ -16,22 +16,22 @@ public class Platform_Tilt : MonoBehaviour
     {
         tiltMax =GetComponent<Transform>().eulerAngles;
         //Up-Arrow
-        if(Input.GetAxis ("Horizontal") > .3 && (tiltMax.z <=6 || tiltMax.z >= 350))
+        if(Input.GetAxis ("Horizontal") > .2 && (tiltMax.z <=6 || tiltMax.z >= 350))
         {
             transform.Rotate(0, 0, 1);
         }
         //Down-Arrow
-        if (Input.GetAxis("Horizontal") < -.3 && (tiltMax.z >= 351 || tiltMax.z <=7))
+        if (Input.GetAxis("Horizontal") < -.2 && (tiltMax.z >= 351 || tiltMax.z <=7))
         {
             transform.Rotate(0, 0, -1);
         }
         //Right-Arrow
-        if (Input.GetAxis("Vertical") > .3 && (tiltMax.x <=8 || tiltMax.x >= 350))
+        if (Input.GetAxis("Vertical") > .2 && (tiltMax.x <=8 || tiltMax.x >= 350))
         {
             transform.Rotate(1, 0, 0);
         }
         //Left-Arrow
-        if (Input.GetAxis("Vertical") < -.3 && (tiltMax.x >= 351 || tiltMax.x <=9))
+        if (Input.GetAxis("Vertical") < -.2 && (tiltMax.x >= 351 || tiltMax.x <=9))
         {
             transform.Rotate(-1, 0, 0);
         }
