f853542bc954238b377733faea4053d441de7408
Ernesto Sanchez <esanchez9@sdsu.edu>
2020-11-30 21:26:16 -0800

    able to make platform tilt but need to set limits

---
+++ Assets/Scripts/Platform_Tilt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Tilt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis ("Horizontal") > .2)
+        {
+            transform.Rotate(0, 0, 1);
+        }
+        if (Input.GetAxis("Horizontal") < -.2)
+        {
+            transform.Rotate(0, 0, -1);
+        }
+        if (Input.GetAxis("Vertical") > .2)
+        {
+            transform.Rotate(1, 0, 0);
+        }
+        if (Input.GetAxis("Vertical") < -.2)
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+    }
+}
